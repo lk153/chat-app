b0c0e576292a686ee38dcb4dd515bfd778918e49
Viet Nguyen <viet.nguyen3@tiki.vn>
2020-01-20 15:21:00 +0700

    Done initialize project

--- server/nodejs/index.js
+++ server/nodejs/index.js
@@ -1,10 +1,23 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const PORT = 3000;
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(PORT, function() {
+    console.log('listening on *:' + PORT);
 });
