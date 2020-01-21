1c2031ab83abfdfee5b5f181a1af61371b23dcb4
Viet Nguyen <viet.nguyen3@tiki.vn>
2020-01-21 12:08:02 +0700

    Chat Demo

--- server/nodejs/index.js
+++ server/nodejs/index.js
@@ -1,20 +1,42 @@
 var app = require('express')();
+const express = require('express');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const PORT = 3000;
+app.use(express.static('public'));
+
+const PORT = 80;
+
+const EVENT_CONNECT = "connect";
+const EVENT_DISCONNECT = "disconnect";
+const EVENT_CHAT_MESSAGE = "chat-message";
+const EVENT_CHANGE_USER = "change-user";
+const EVENT_REGISTER_USER = "register-user";
+const EVENT_REGISTERED_USER = "registered-user";
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/chat', function(req, res) {
+    res.sendFile(__dirname + '/chat.html');
+});
 
-io.on('connection', function(socket) {
+io.on(EVENT_CONNECT, function(socket) {
+    socket.username = "Anonymous";
     console.log('a user connected');
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+
+    socket.on(EVENT_CHAT_MESSAGE, function(msg) {
+        msg.username = socket.username;
+        io.emit(EVENT_CHAT_MESSAGE, msg);
     });
 
-    socket.on('disconnect', function() {
+    socket.on(EVENT_DISCONNECT, function() {
         console.log('user disconnected');
+    });
+
+    socket.on(EVENT_REGISTER_USER, function(msg) {
+        socket.username = msg.username;
+        io.emit(EVENT_REGISTERED_USER, msg);
     });
 });
 
