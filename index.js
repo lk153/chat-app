1e3b5895c945dee9687b27081398fbf82db1abbe
Viet Nguyen <viet.nguyen3@tiki.vn>
2020-01-21 17:15:31 +0700

    Change expose port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 app.use(express.static('public'));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const EVENT_CONNECT = "connect";
 const EVENT_DISCONNECT = "disconnect";
